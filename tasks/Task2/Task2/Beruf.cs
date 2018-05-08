b48203c953eebdcba97a91b88071dc0282e5bcef
Roland Varga <rv.varga@gmail.com>
2018-05-08 22:42:52 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Beruf.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Beruf
+    {
+        private string bezeichnung;
+        private bool ausgeschrieben;
+
+        public Beruf(string bezeichnung, UInt32 jahresGehalt) {
+
+            this.Bezeichnung = bezeichnung;
+            this.JahresGehalt = jahresGehalt;
+            this.Ausgeschrieben = false;
+        }
+
+        public string Bezeichnung {
+
+            get { return this.bezeichnung; }
+
+            private set {
+
+                if (value == null || value.Length < 3)
+                    throw new Exception("Invalid ArgumentException!");
+
+                this.bezeichnung = value; }
+        }
+
+        public UInt32 JahresGehalt { get; set; }
+
+        public bool Ausgeschrieben {
+
+            get { return this.ausgeschrieben; }
+
+            private set { this.ausgeschrieben = value; }
+
+        } 
+
+        public void Ausschreiben(bool ausgeschrieben) { this.Ausgeschrieben = ausgeschrieben;   } 
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beruf beruf = new Beruf("Mechaniker", 30000);
+
+            Console.WriteLine("Beruf: {0} Jahresgehalt: {1} Ausgeschrieben: {2}", beruf.Bezeichnung, beruf.JahresGehalt, beruf.Ausgeschrieben);
+
+            beruf.Ausschreiben(true);
+
+            Console.WriteLine("Beruf: {0} Jahresgehalt: {1} Ausgeschrieben: {2}", beruf.Bezeichnung, beruf.JahresGehalt, beruf.Ausgeschrieben);
+
+        }
+    }
+}
