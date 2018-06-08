a30de01171728684572d21358abdeea7e289afbc
Roland Varga <rv.varga@gmail.com>
2018-06-08 10:38:16 +0200

    add project Task6

---
+++ tasks/Task6/Task6/Task6/Beruf.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public class Beruf : IBeruf
+    {
+        private string bezeichnung;
+        private bool ausgeschrieben;
+
+        public Beruf(string bezeichnung, UInt32 jahresGehalt)
+        {
+            if (bezeichnung == null) { throw new ArgumentNullException("parameter is null."); }
+
+            SetBezeichnung(bezeichnung);
+            JahresGehalt = jahresGehalt;
+            ausgeschrieben = false;
+        }
+
+        #region properties
+
+        public string Bezeichnung
+        {
+
+            get { return this.bezeichnung; }
+        }
+
+        public bool Ausgeschrieben
+        {
+
+            get { return this.ausgeschrieben; }
+        }
+
+        public UInt32 JahresGehalt { get; set; }
+
+        #endregion
+
+        #region methods
+
+        private void SetBezeichnung(string bezeichnung)
+        {
+
+            if (bezeichnung == null || bezeichnung.Length < 3)
+                throw new Exception("Invalid ArgumentException!");
+
+            this.bezeichnung = bezeichnung;
+        }
+
+        public void Ausschreiben(bool ausgeschrieben) { this.ausgeschrieben = ausgeschrieben; }
+
+        #endregion
+    }
+}
---
+++ tasks/Task6/Task6/Task6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd    = new Random();
+            var    beruf  = new Subject<Beruf>();
+
+            beruf.Subscribe(x => Console.WriteLine($"Bezeichnung: {x.Bezeichnung}; Jahresgehalt: {x.JahresGehalt}"));
+
+            //Console.WriteLine("------------- Ohne Filter -------------");
+            for (var i = 1; i <= 20; i++)
+            {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(.5));
+                beruf.OnNext(new Beruf(String.Format("Beruf {0}", i.ToString()) , (UInt32)rnd.Next(20000 , 20000 * i)));
+            }            
+        }
+    }
+}
