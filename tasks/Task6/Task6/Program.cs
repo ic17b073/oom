9fc753312f9a8a990fae8d00813b4f466d1af70b
Roland Varga <rv.varga@gmail.com>
2018-06-14 11:33:42 +0200

    add project Task7 / bugfix project Task6

--- tasks/Task6/Task6/Program.cs
+++ tasks/Task6/Task6/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
@@ -12,17 +13,19 @@ namespace Task6
     {
         static void Main(string[] args)
         {
-            Random rnd    = new Random();
-            var    beruf  = new Subject<Beruf>();
+            var rnd = new Random();
+            var beruf = new Subject<Beruf>();
 
             beruf.Subscribe(x => Console.WriteLine($"Bezeichnung: {x.Bezeichnung}; Jahresgehalt: {x.JahresGehalt}"));
 
             //Console.WriteLine("------------- Ohne Filter -------------");
-            for (var i = 1; i <= 20; i++)
+            for (var i = 1; i <= 50; i++)
             {
                 System.Threading.Thread.Sleep(TimeSpan.FromSeconds(.5));
-                beruf.OnNext(new Beruf(String.Format("Beruf {0}", i.ToString()) , (UInt32)rnd.Next(20000 , 20000 * i)));
-            }            
-        }
+                beruf.OnNext(new Beruf(String.Format("Beruf {0}", i.ToString()), (UInt32)rnd.Next(20000, 20000 * i)));
+            }
+        
+            Console.ReadLine();
+        }        
     }
 }
---
+++ tasks/Task7/Task7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<IBeruf> jobs = new List<IBeruf>();            
+            var createTask = CreateJobs(jobs);
+            var calcTask = CalculateJobs(jobs);
+            
+            Console.WriteLine("doing something else ...");
+            Console.ReadLine();            
+        }
+                
+
+        public static Task<bool> TimeOut()
+        {
+            return Task.Run(() =>
+            {
+                for (var i = 0; i < 3; i++)
+                {
+                    Task.Delay(TimeSpan.FromSeconds(1.0)).Wait();
+                }
+
+                return true;
+
+            });
+        }
+
+        public static async Task CreateJobs(List<IBeruf> jobs)
+        {
+            var random = new Random();
+            var name = String.Empty;
+            var salary = 0;
+
+            for (var i = 0; i < Int16.MaxValue; i++) {
+
+                name = String.Format("Beruf_{0}", i.ToString());
+                salary = random.Next(100000, 1000000);
+                
+                Console.WriteLine("[P] Create new job: {0} Salary: {1}", name, salary.ToString());
+
+                jobs.Add(new Beruf(name, (uint)salary));
+
+                if (await TimeOut()) Console.WriteLine($"[P] New job created");
+            }
+        }
+
+        public static async Task CalculateJobs(List<IBeruf> jobs)
+        {
+            for (var i = 0; i < Int16.MaxValue; i++)
+            {
+
+                Console.WriteLine($"[T] Calculation jobs");
+                
+                if (await TimeOut()) Console.WriteLine("[T] {0} jobs found. [jobs: {1}]", (jobs.Count != 0 ? "" : "No ") , jobs.Count);
+            }
+        }
+    }
+}
